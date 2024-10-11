f7a194bbeacefc21284a0c1f644a96e658ca3100
Davide Filosa <davideclaypool@gmail.com!>
2024-10-11 13:47:20 +0200

    created auth screen

---
+++ src/components/logo.tsx
@@ -0,0 +1,13 @@
+import { Target } from "lucide-react";
+import React from "react";
+
+export const Logo = () => {
+  return (
+    <div>
+      <div className="text-[#2563eb] flex gap-2 items-center text-xl font-bold">
+        <Target className="size-6" />
+        TRACKTIVE
+      </div>
+    </div>
+  );
+};
---
+++ src/features/auth/components/sign-up-card.tsx
@@ -0,0 +1,146 @@
+"use client";
+
+import { DottedSeparator } from "@/components/dotted-separator";
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import React from "react";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+
+const formSchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z
+    .string()
+    .min(8, "Your password should be at least 8 characters long"),
+});
+
+export const SignUpCard = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    defaultValues: { name: "", email: "", password: "" },
+    resolver: zodResolver(formSchema),
+  });
+
+  const onSubmit = (values: z.infer<typeof formSchema>) => {
+    console.log(values);
+  };
+
+  return (
+    <Card className="w-full h-full md:w-[487px] border-none shadow-none">
+      <CardHeader className="flex items-center justify-center text-center p-7">
+        <CardTitle className="text-2xl">Sign Up</CardTitle>
+        <CardDescription>
+          By signing up, you agree to our{" "}
+          <Link href={"/privacy"}>
+            <span className="text-blue-700">Privacy Policy</span>
+          </Link>{" "}
+          and{" "}
+          <Link href={"/terms"}>
+            <span className="text-blue-700">Terms of Services</span>
+          </Link>
+        </CardDescription>
+      </CardHeader>
+      <div className="px-7 mb-2">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7">
+        <Form {...form}>
+          <form className="space-y-4" onSubmit={form.handleSubmit(onSubmit)}>
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="text"
+                      placeholder="Enter your name"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="email"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      type="email"
+                      placeholder="Enter email address"
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormControl>
+                    <Input
+                      type="password"
+                      placeholder="Enter password"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button disabled={false} size="lg" className="w-full">
+              Sign Up
+            </Button>
+          </form>
+        </Form>
+      </CardContent>
+      <div className="px-7 mb-2">
+        <DottedSeparator />
+      </div>
+      <CardContent className="p-7 flex flex-col gap-y-4">
+        <Button
+          variant={"secondary"}
+          size={"lg"}
+          disabled={false}
+          className="w-full"
+        >
+          <FcGoogle className="mr-2 size-5" /> Login with Google
+        </Button>
+        <Button
+          variant={"secondary"}
+          size={"lg"}
+          disabled={false}
+          className="w-full"
+        >
+          <FaGithub className="mr-2 size-5" />
+          Login with Github
+        </Button>
+      </CardContent>
+    </Card>
+  );
+};
