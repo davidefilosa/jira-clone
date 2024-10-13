6926fc931b4570bbe46e2c4463c4c6002cd61a4e
Davide Filosa <davideclaypool@gmail.com!>
2024-10-13 10:03:48 +0200

    create workspace functionality

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,9 +1,14 @@
 import { getCurrent } from "@/features/auth/action";
+import { CreateWorkspaceForm } from "@/features/workspaces/components/create-workspace-form";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
-  return <div>Home page </div>;
+  return (
+    <div>
+      <CreateWorkspaceForm />
+    </div>
+  );
 }
